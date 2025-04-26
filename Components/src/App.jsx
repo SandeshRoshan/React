51aef51cbb1d9204baf83096a70835c1a341fb5b
SandeshRoshan <srmilaptop@outlook.com>
2025-04-26 11:10:20 +0530

    26April25

--- Components/src/App.jsx
+++ Components/src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>What are components</h1>
+      <li>Components are indepindent and resuble bits on code.</li>
+      <li>Components serve same as purpose as js function </li>
+    </div> 
+
+         <h2>There are 2 types of componects </h2>
+         <h3><li>Class Based</li>      <li>Function Based</li></h3>
     </>
   )
 }
